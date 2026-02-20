7521dcbe7a9c4522ec8cf91dc3dc69bc31383883
kou342156 <kousonglin@gmail.com>
2026-02-21 00:49:16 +0900

    fix: サーバーとクライアントのコンフリクト対策のために追加

---
+++ kimetabi/lib/actions.ts
@@ -0,0 +1,8 @@
+// lib/actions.ts
+"use server"
+
+import { signIn } from "../auth"; // 階層に合わせて修正（@/auth でもいけるはずです）
+
+export async function handleGoogleSignIn() {
+   await signIn("google", { redirectTo: "/" });
+}
