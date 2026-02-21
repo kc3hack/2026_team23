2e58e4cea0c6797dfdd4f65552ab4e8f120e7b26
kou342156 <kousonglin@gmail.com>
2026-02-21 11:33:40 +0900

    docs:移動させるため消した

--- kimetabi/lib/actions.ts
+++
@@ -1,8 +0,0 @@
-// lib/actions.ts
-"use server"
-
-import { signIn } from "../auth"; // 階層に合わせて修正（@/auth でもいけるはずです）
-
-export async function handleGoogleSignIn() {
-   await signIn("google", { redirectTo: "/" });
-}
